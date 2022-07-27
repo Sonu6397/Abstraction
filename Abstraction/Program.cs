5c2824c225a18746ba90c4f55b0ab4729588ad77
admin <admin@CHETUIWK1860>
2022-07-27 14:19:54 +0530

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class Example
+    {
+        public abstract void MyMethod();
+        
+        public void Add()
+        {
+            Console.WriteLine("method is non-static");
+        }
+
+    }
+
+    class Demo : Example
+    {
+        public override void MyMethod()
+        {
+            Console.WriteLine("this method is MyMethod");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            d.MyMethod();
+            d.Add();
+            Console.ReadLine();
+
+        }
+    }
+}
